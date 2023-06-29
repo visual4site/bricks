0a4d71d414110115f6dde6682e7a8c885cb81013
Bagus <bagus.haig@bluemoondigital.co>
2023-06-29 16:17:53 -0600

    Update pill-in-pill.js

--- pill-in-pill.js
+++ pill-in-pill.js
@@ -1,21 +1,11 @@
 // DDX Bricks Wiki - See https://developer.domo.com/docs/ddx-bricks/getting-started-using-ddx-bricks
 // for tips on getting started, linking to Domo data and debugging your app
  
-//Available globals
-var domo = window.domo; // For more on domo.js: https://developer.domo.com/docs/dev-studio-guides/domo-js#domo.get
-var datasets = window.datasets;
 
 //Step 1. Select your dataset(s) from the button in the bottom left corner
-var url_string = window.location.href; // www.test.com?filename=test
-    var url = new URL(url_string);
-    var paramValue = url.searchParams.get("hi");
-    console.log(paramValue);
 
 
 //Step 2. Query your dataset(s): https://developer.domo.com/docs/dev-studio-references/data-api
-var fields = ['state', 'revenue'];
-var groupby = ['state'];
-var query = `/data/v1/${datasets[0]}?fields=${fields.join()}&groupby=${groupby.join()}`;
 domo.get(query).then(handleResult);
 
 
